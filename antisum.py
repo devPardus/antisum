0917b5cd400081d363336f189b4907c0fe1878fb
Jonathan Moon <73129897+devPardus@users.noreply.github.com>
2024-09-09 13:16:19 +0900

    Create antisum.py

---
+++ antisum.py
@@ -0,0 +1,10 @@
+def find_combinations(m, start=1):
+    if m == 0:
+        return [[]]
+    
+    combinations = []
+    for i in range(start, m + 1):
+        for sub_combination in find_combinations(m - i, i + 1):
+            combinations.append([i] + sub_combination)
+    
+    return combinations
